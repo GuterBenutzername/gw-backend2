7fe312b443a514f240d932373edd74d56f9c7451
Adam Y. Cole II <afewburritos@gmail.com>
2024-12-19 15:46:47 -0600

    asdf

--- initial_migration.sql
+++ initial_migration.sql
@@ -1,14 +1,13 @@
 CREATE SCHEMA app_public;
+CREATE ROLE graphql_visitor;
 
 CREATE TABLE IF NOT EXISTS app_public.courses (
        pk SERIAL NOT NULL PRIMARY KEY,
-       id UUID NOT NULL UNIQUE,
        name VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS app_public.assignments (
        pk SERIAL NOT NULL PRIMARY KEY,
-       id UUID NOT NULL UNIQUE,
        name VARCHAR(100),
        grade NUMERIC(5, 2),
        weight NUMERIC(3, 3),
@@ -16,10 +15,13 @@ CREATE TABLE IF NOT EXISTS app_public.assignments (
        FOREIGN KEY (course_id) REFERENCES app_public.courses(pk)
 );
 
-GRANT SELECT (id,name,grade,weight,course_id) ON assignments TO graphql_visitor;
-GRANT UPDATE (name,grade,weight,course_id) ON assignments TO graphql_visitor;
-GRANT DELETE ON assignments TO graphql_visitor;
+GRANT SELECT (name,grade,weight,course_id) ON app_public.assignments TO graphql_visitor;
+GRANT UPDATE (name,grade,weight,course_id) ON app_public.assignments TO graphql_visitor;
+GRANT DELETE ON app_public.assignments TO graphql_visitor;
 
-GRANT SELECT (id,name) ON courses TO graphql_visitor;
-GRANT UPDATE (name) ON courses TO graphql_visitor;
-GRANT DELETE ON courses TO graphql_visitor;
+GRANT SELECT (name) ON app_public.courses TO graphql_visitor;
+GRANT UPDATE (name) ON app_public.courses TO graphql_visitor;
+GRANT DELETE ON app_public.courses TO graphql_visitor;
+
+INSERT INTO app_public.courses VALUES (DEFAULT, DEFAULT, 'hello!');
+INSERT INTO app_public.assignments VALUES (DEFAULT, DEFAULT, 'hiya', 100.00, 0.25, (SELECT pk FROM app_public.courses ORDER BY id LIMIT 1));
